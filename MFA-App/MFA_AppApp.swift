9b5066ef86da94a76e7f823d024c30a894845c63
Balqis Shafira Aini <balqisshafiraaini@gmail.com>
2026-08-16 16:18:35 +0700

    Initial Commit

---
+++ MFA-App/MFA_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MFA_AppApp.swift
+//  MFA-App
+//
+//  Created by Balqis Shafira Aini on 16/08/26.
+//
+
+import SwiftUI
+
+@main
+struct MFA_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
